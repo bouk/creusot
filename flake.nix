0fdff3fc923dc79f461c0b568f6184a142685c92
Bouke van der Bijl <i@bou.ke>
2022-04-14 15:00:27 +0200

    Add some solvers
    I was getting a compilation error for cvc4

--- flake.nix
+++ flake.nix
@@ -8,13 +8,22 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay }: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlay ]; };
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ rust-overlay.overlay ];
+      config.allowUnfree = true;
+    };
+
+    rust = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain);
+
     in rec {
-      devShell = with pkgs; mkShell {
-        buildInputs = [
-          (rust-bin.fromRustupToolchainFile ./rust-toolchain)
-          zlib
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          alt-ergo
+          rust
           why3
+          z3
+          zlib
         ];
       };
     }
